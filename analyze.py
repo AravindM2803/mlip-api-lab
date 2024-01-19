6cb83be9052ebdb4bcc48907f6c21620ab23f6cc
Aravind <aravindsmysore@gmail.com>
2024-01-19 17:52:15 -0500

    analyze.py: added endpoint and key

--- analyze.py
+++ analyze.py
@@ -1,10 +1,11 @@
+import os
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+endpoint = os.getenv("AZURE_ENDPOINT")
+key = os.getenv("AZURE_KEY")
 
 credentials = CognitiveServicesCredentials(key)
 
